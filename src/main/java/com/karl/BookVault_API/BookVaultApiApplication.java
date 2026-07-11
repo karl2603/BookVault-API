40d8668d3fed3c61c2330d3fc675632046d19fa1
karl2603 <arvindrajkarl@gmail.com>
2026-07-11 11:39:31 +0530

    Project setup

---
+++ src/main/java/com/karl/BookVault_API/BookVaultApiApplication.java
@@ -0,0 +1,13 @@
+package com.karl.BookVault_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookVaultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookVaultApiApplication.class, args);
+	}
+
+}
